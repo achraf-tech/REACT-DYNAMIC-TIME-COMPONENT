b9ab9d1e7f409d6d03ea82002e52d1b3a797da3c
achraf-tech <benkadaachref@gmail.com>
2019-11-27 10:01:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+let t = 100515515;
+let h = parseInt(t / 3600000);
+let m = parseInt((t % 3600000) / 60000);
+let s = parseInt(((t % 3600000) % 60000) / 1000);
+ 
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{h.toString().padStart(2, 0) + ":" + m.toString().padStart(2, 0) + ":" + s.toString().padStart(2, 0)}</h1>
     </div>
   );
 }
